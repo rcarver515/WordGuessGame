4a94529c6ff07b2034c5ac631bf1e2e995a17801
Robert Grant <rcarver515@gmail.com>
2019-02-12 23:44:37 -0600

    Javascript Word Guess Game HW

---
+++ assets/javascript/game.js
@@ -0,0 +1,46 @@
+  
+    
+     var words = [
+     "software",
+     "data",
+     "development",
+     "bootstrap",
+     "array"
+     ];
+     
+     var word = words[Math.floor(Math.random() * words.length)];
+     
+     var answerArray = [];
+     for (var i = 0; i < word.length; i++) {
+     answerArray[i] = "_";
+     }
+     var remainingLetters = word.length;
+     
+     while (remainingLetters > 0) {
+     
+     alert(answerArray.join(" "));
+     
+     var guess = prompt("Guess a letter, or click Cancel to stop playing.");
+     if (guess === null) {
+  
+     break;
+     } else if (guess.length !== 1) {
+     alert("Please enter a single letter.");
+     } else {
+     
+     for (var j = 0; j < word.length; j++) {
+     if (word[j] === guess) {
+     answerArray[j] = guess;
+     remainingLetters--;
+     }
+     }
+    }
+    
+     
+
+
+alert(answerArray.join(" "));
+alert("Good job! The answer was " + word);
+
+    };
+  
